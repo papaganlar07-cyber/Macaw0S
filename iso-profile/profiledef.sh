e4acdd9ba3e53158040bf83f9f29911c5768cae7
papaganlar07-cyber <papaganlar07@gmail.com>
2026-05-01 18:45:01 +0300

    Resolve merge-conflict-prone ArchISO files with canonical names and bootmodes

--- iso-profile/profiledef.sh
+++ iso-profile/profiledef.sh
@@ -4,15 +4,11 @@ iso_name="macaw0s"
 iso_label="MACAW0S"
 iso_publisher="Macaw0S <https://macaw0s.local>"
 iso_application="Macaw0S Workstation Live ISO"
-iso_version="0.3.1"
+iso_version="0.3.2"
 install_dir="arch"
 arch="x86_64"
 
-# Supported boot modes only (no systemd-boot/efiboot)
-bootmodes=(
-  'bios.syslinux.mbr'
-  'uefi-x64.grub.esp'
-)
+bootmodes=('bios.syslinux' 'uefi.grub')
 
 buildmodes=('iso')
 pacman_conf="pacman.conf"
